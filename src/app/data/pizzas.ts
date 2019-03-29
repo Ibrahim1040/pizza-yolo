a09a8c3d746ebd826ec40e260179afbdbed4bcfb
Ibrahim Aharrar <we18ahib@fijstgilles.local>
2019-03-29 16:31:24 +0100

    Premier commit de mon projet pizzeria en angular

---
+++ src/app/data/pizzas.ts
@@ -0,0 +1,56 @@
+export const pizzas = [
+  { 
+    nom: "Vegetariana", 
+    prix: 12, 
+    ingredients: ["tomate", "courgette"], 
+    image: "vegetariana.jpeg" 
+  }, 
+  { 
+    nom: "Diavolo", 
+    prix: 13, 
+    ingredients: ["tomate", "chorizo"], 
+    image: "diavolo.jpeg"  
+  },
+  { 
+    nom: "Parmeggiano", 
+    prix: 9, 
+    ingredients: ["tomate", "parmesan"], 
+    image: "parmegiano.jpeg" 
+  }, 
+  { 
+      nom: "4 fromages", 
+    prix: 14, 
+    ingredients: ["crème", "gorgonzola"], 
+    image: "4f.jpeg"  
+  },   
+  { 
+    nom: "Poulet", 
+    prix: 13, 
+    ingredients: ["tomate", "courgette"], 
+    image: "poulet.jpeg" 
+  },
+   { 
+    nom: "fruit", 
+    prix: 25, 
+    ingredients: ["tomate", "courgette","fruit"], 
+    image: "poulet.jpeg" 
+  },
+   { 
+      nom: "4 fromages", 
+    prix: 14, 
+    ingredients: ["crème", "gorgonzola"], 
+    image: "4f.jpeg"  
+  },   
+  { 
+    nom: "Poulet", 
+    prix: 13, 
+    ingredients: ["tomate", "courgette"], 
+    image: "poulet.jpeg" 
+  },
+   { 
+    nom: "fruit", 
+    prix: 25, 
+    ingredients: ["tomate", "courgette","fruit"], 
+    image: "poulet.jpeg" 
+  }
+];
